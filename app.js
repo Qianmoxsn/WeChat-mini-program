61f82e2f61aaf0faaccf58207ff9ed7c40a68644
Qianmo <92790790+Qianmoxsn@users.noreply.github.com>
2023-06-04 22:22:12 +0800

    Update app.js

--- app.js
+++ app.js
@@ -1,31 +1,35 @@
 // app.js
 App({
-  onLaunch() {
-    // 展示本地存储能力
-    const logs = wx.getStorageSync('logs') || []
-    logs.unshift(Date.now())
-    wx.setStorageSync('logs', logs)
+    onLaunch() {
+        // 展示本地存储能力
+        const logs = wx.getStorageSync('logs') || []
+        logs.unshift(Date.now())
+        wx.setStorageSync('logs', logs)
 
-    // 登录
-    wx.login({
-      success: res => {
-        // 发送 res.code 到后台换取 openId, sessionKey, unionId
-      }
-    })
-
-    //建立ws连接
-    wx.connectSocket({
-      url: 'ws://10.122.234.77:8080',
-      success: (result) => {
-        console.log(result);
-        wx.sendSocketMessage({
-          data: '[C]You are connected with Wechat mini app',
+        // 登录
+        wx.login({
+            success: res => {
+                // 发送 res.code 到后台换取 openId, sessionKey, unionId
+            }
         })
-      }
-    })
 
-  },
-  globalData: {
-    userInfo: null
-  }
+        //建立ws连接
+        wx.connectSocket({
+            url: 'ws://10.122.234.77:8080',
+            success: (result) => {
+                console.log(result);
+
+            }
+        })
+
+        wx.onSocketOpen(function (res) {
+            wx.sendSocketMessage({
+                data: '[C]You are connected with Wechat mini app',
+            })
+        })
+
+    },
+    globalData: {
+        userInfo: null
+    }
 })
